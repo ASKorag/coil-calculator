34fac0a57672bc31d8083e62960470ef81e6f92a
Andrey Serikov <AS.Korag@protonmail.com>
2021-09-08 15:47:19 +0300

    fix: MainPage (structure & styles)

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,11 +3,14 @@ import './MainPage.sass'
 
 export const MainPage = () => {
   return (
-    <>
+    <div className="main-page page">
       <div className="main-page__wrap">
-        <Button text="New coil" mod="new" href="/calc"/>
-        <Button text="Open coil" mod="open" />
+        <h1 className="main-page__title">Welcome!</h1>
+        <div className="main-page__actions">
+          <Button text="New coil" mod="new" href="/calc"/>
+          <Button text="Open coil" mod="open"/>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
